5b006f14d1ac7eab64e4ca0d2b3bc985bd733969
Moti-Jnr <kipchimoti@gmail.com>
2025-03-18 15:59:39 +0300

    login

--- main_app/views.py
+++ main_app/views.py
@@ -7,6 +7,7 @@ from django.db.models import Q, Sum
 from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 
+from .app_forms import CustomerForm, DepositForm, LoginForm
 #from .app_forms import CustomerForm, DepositForm, LoginForm
 from .models import Customer, Deposit
 
